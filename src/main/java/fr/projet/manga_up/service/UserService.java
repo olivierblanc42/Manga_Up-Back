868e0c3697676ec4ffd16cb946ddd73013e269c3
SMARCOCCIA <seb@gmail.fr>
2024-07-03 11:48:57 +0200

    Création de la méthode getUser

--- src/main/java/fr/projet/manga_up/service/UserService.java
+++ src/main/java/fr/projet/manga_up/service/UserService.java
@@ -1,7 +1,32 @@
 package fr.projet.manga_up.service;
 
+import fr.projet.manga_up.dao.UserDao;
+import fr.projet.manga_up.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Component
 public class UserService {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    public User getUser(Integer id){
+        LOGGER.info("getUser");
+        Optional<User> ou = userDao.findById(id);
+        LOGGER.info("User : {}", ou);
+        if(ou.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aucun utilisateur n'a été trouvé.");
+        }else {
+            return ou.get();
+        }
+    }
 }
