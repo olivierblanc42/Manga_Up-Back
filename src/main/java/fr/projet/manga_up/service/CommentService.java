8849ab87428d4554670eed15e7a3d3d8d3ed3d52
SMARCOCCIA <seb@gmail.fr>
2024-07-14 20:44:51 +0200

    Ajout d'un logger, ajout dans la méthode getCommentsByIdManga d'un paramètre pageable, création d'une m'éthode pour récupérer le nombre de note d'un manga

--- src/main/java/fr/projet/manga_up/service/CommentService.java
+++ src/main/java/fr/projet/manga_up/service/CommentService.java
@@ -1,20 +1,35 @@
 package fr.projet.manga_up.service;
 
-import java.util.List;
-
+import fr.projet.manga_up.controller.MangaController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import fr.projet.manga_up.dao.CommentDao;
 import fr.projet.manga_up.model.Comment;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class CommentService {
-	
+	private static final Logger LOGGER= LoggerFactory.getLogger(MangaController.class);
+
 	@Autowired
 	private CommentDao commentDao;
 	
-	public List<Comment> getCommentsByIdManga(Integer id) {
-		return commentDao.findAllById(id);
+	public Page<Comment> getCommentsByIdManga(Integer id, Pageable pageable) {
+		LOGGER.info("Méthode getCommentsByIdManga, pageable : {}", pageable);
+		return commentDao.findAllById(id, pageable);
+	}
+
+	public List<Integer> findAllRatingByIdManga(Integer idManga){
+		LOGGER.info("Méthode findAllRatingByIdManga");
+		return commentDao.findAllRatingByIdManga(idManga);
+
 	}
 }
