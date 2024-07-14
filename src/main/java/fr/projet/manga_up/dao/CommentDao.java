1e86100446cd50942d98d1a7e15696051fb11bee
SMARCOCCIA <seb@gmail.fr>
2024-07-14 20:41:49 +0200

    Ajout d'une méthode pour récupérer toutes les notes d'un manga

--- src/main/java/fr/projet/manga_up/dao/CommentDao.java
+++ src/main/java/fr/projet/manga_up/dao/CommentDao.java
@@ -1,7 +1,7 @@
 package fr.projet.manga_up.dao;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,8 +9,14 @@ import fr.projet.manga_up.model.Comment;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CommentDao extends CrudRepository<Comment, Integer> {
-	@Query(value="SELECT * FROM comment c WHERE c.id_manga = :id", nativeQuery=true)
-	List<Comment> findAllById(@Param("id") Integer id);
+	@Query("FROM Comment c WHERE c.manga.id = :idManga")
+	Page<Comment> findAllById(@Param("idManga") Integer idManga, Pageable pageable);
+
+	@Query(value = "SELECT c.rating FROM comment  c WHERE c.id_manga = :idManga", nativeQuery = true)
+	List<Integer> findAllRatingByIdManga(@Param("idManga") Integer idManga);
 }
