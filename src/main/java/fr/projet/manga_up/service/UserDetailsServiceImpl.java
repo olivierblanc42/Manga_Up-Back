7fb0f06f41397cb46b15d3aeb3b207c2e8de01d4
SMARCOCCIA <seb@gmail.fr>
2024-09-12 19:20:51 +0200

    Remplacement de User par AppUser

--- src/main/java/fr/projet/manga_up/service/UserDetailsServiceImpl.java
+++ src/main/java/fr/projet/manga_up/service/UserDetailsServiceImpl.java
@@ -1,12 +1,17 @@
-package fr.projet.manga_up.security.config;
+package fr.projet.manga_up.service;
 
 import fr.projet.manga_up.dao.UserDao;
-import fr.projet.manga_up.model.User;
+import fr.projet.manga_up.model.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -15,12 +20,18 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserDao userDao;
 
     @Override
-    public UserDetails loadUserByUsername(String userName) {
-        User user = userDao.findByUsername(userName);
+    public UserDetails loadUserByUsername(String username) {
+        AppUser user = userDao.findByUsername(username);
         if (user == null) {
-            throw new UsernameNotFoundException(userName);
+            throw new UsernameNotFoundException(String.format("L'utilisateur %s n'a pas été trouvé", username));
         }
-        return (UserDetails) user;
+        List<SimpleGrantedAuthority> authorities=user.getRoles().stream()
+                .map(r->new SimpleGrantedAuthority(r.getRole())).collect(Collectors.toList());
+        UserDetails userDetails= User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(authorities).build();
+        return userDetails;
     }
 }
 
