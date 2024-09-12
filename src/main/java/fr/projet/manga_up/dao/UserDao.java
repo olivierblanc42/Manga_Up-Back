850e8f589b61a4db274068db5052fbfd9e302d6e
SMARCOCCIA <seb@gmail.fr>
2024-09-12 19:06:33 +0200

    Changement du nom de l'entité User par AppUser

--- src/main/java/fr/projet/manga_up/dao/UserDao.java
+++ src/main/java/fr/projet/manga_up/dao/UserDao.java
@@ -1,21 +1,22 @@
 package fr.projet.manga_up.dao;
 
-import fr.projet.manga_up.model.Manga;
-import fr.projet.manga_up.model.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import java.util.List;
+import java.util.Set;
+
+import fr.projet.manga_up.model.AppRole;
+import fr.projet.manga_up.model.AppUser;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 @Transactional
 @Repository
-public interface UserDao extends CrudRepository<User, Integer> {
+public interface UserDao extends CrudRepository<AppUser, Integer> {
 
 
     //User getUserByUsernameAndPassword(String username, String password);
@@ -24,10 +25,10 @@ public interface UserDao extends CrudRepository<User, Integer> {
     @Query(value = "SELECT Id_manga FROM user_manga WHERE id_user= :id", nativeQuery = true)
     List<Integer> getAllMangaByUserId(@Param("id") Integer id);
 
-    User findByUsername(String userName);
+    AppUser findByUsername(String username);
 
+    AppUser findByEmail(String email);
 
-    @Query(  "FROM User  ")
-    List<User> findAllUsers();
-
+    @Query( "FROM AppUser  ")
+    List<AppUser> findAllUsers();
 }
