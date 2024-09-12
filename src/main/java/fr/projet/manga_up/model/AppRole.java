75ed3e58f685d29b65d47bc0df332a3bb026dc7e
SMARCOCCIA <seb@gmail.fr>
2024-09-12 19:10:17 +0200

    Ajout d'une entité Approle

--- src/main/java/fr/projet/manga_up/model/AppRole.java
+++ src/main/java/fr/projet/manga_up/model/AppRole.java
@@ -1,20 +1,30 @@
 package fr.projet.manga_up.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-public class Role {
+@Table(name = "role", schema = "manga_up")
+public class AppRole {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "role", nullable = false, length = 15)
     private String role;
 
-    public Role() {
-        super();
+    @ManyToMany(mappedBy = "roles")
+    private Set<AppUser> users = new HashSet<>();
+
+    public Integer getId() {
+        return id;
     }
 
-    public Role(String role) {
-        super();
-        this.role=role;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getRole() {
@@ -23,5 +33,22 @@ public class Role {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public AppRole() {
+        super();
+    }
+
+    public AppRole(String role) {
+        super();
+        this.role=role;
+    }
+
+    public Set<AppUser> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<AppUser> users) {
+        this.users = users;
     }
 }
