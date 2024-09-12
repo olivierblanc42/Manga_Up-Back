b8042df4619e51623e67ba04e95493ef1f24a050
SMARCOCCIA <seb@gmail.fr>
2024-09-12 19:05:37 +0200

    Ajout d'une méthode pour récupérer un rôle

--- src/main/java/fr/projet/manga_up/dao/AppRoleDao.java
+++ src/main/java/fr/projet/manga_up/dao/AppRoleDao.java
@@ -1,2 +1,12 @@
-package fr.projet.manga_up.dao;public interface AppRoleDao {
+package fr.projet.manga_up.dao;
+
+import fr.projet.manga_up.model.Address;
+import fr.projet.manga_up.model.AppRole;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface AppRoleDao extends CrudRepository<AppRole, String> {
+    @Query(value="SELECT * FROM role ar WHERE ar.role= :role", nativeQuery = true)
+    AppRole findByRole(@Param("role") String role);
 }
