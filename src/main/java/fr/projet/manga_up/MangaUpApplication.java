75acfe0a41a4af42e88b7685da802acbb423c2da
SMARCOCCIA <seb@gmail.fr>
2024-09-12 18:59:45 +0200

    Ajout du passwordEncoder

--- src/main/java/fr/projet/manga_up/MangaUpApplication.java
+++ src/main/java/fr/projet/manga_up/MangaUpApplication.java
@@ -2,6 +2,9 @@ package fr.projet.manga_up;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MangaUpApplication {
@@ -10,4 +13,6 @@ public class MangaUpApplication {
 		SpringApplication.run(MangaUpApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 }
