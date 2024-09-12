7b866d686d493faaab88529fc7c6473fe8ddf4f7
SMARCOCCIA <seb@gmail.fr>
2024-09-12 19:08:29 +0200

    Ajout d'une méthode toEntityRegister

--- src/main/java/fr/projet/manga_up/mapper/UserMapper.java
+++ src/main/java/fr/projet/manga_up/mapper/UserMapper.java
@@ -3,11 +3,11 @@ package fr.projet.manga_up.mapper;
 import fr.projet.manga_up.dao.AddressDao;
 import fr.projet.manga_up.dao.GenderDao;
 import fr.projet.manga_up.dao.UserDao;
+import fr.projet.manga_up.dto.RegisterDTO;
 import fr.projet.manga_up.dto.UserDto;
 import fr.projet.manga_up.model.Address;
-import fr.projet.manga_up.model.Category;
 import fr.projet.manga_up.model.Gender;
-import fr.projet.manga_up.model.User;
+import fr.projet.manga_up.model.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +20,19 @@ public class UserMapper {
     @Autowired
     private GenderDao genderDao;
 
-    public UserDto toDto(User user) {
+    public RegisterDTO toDtoRegister(AppUser user) {
+        return new RegisterDTO(
+                user.getUsername(),
+                user.getEmail(),
+                user.getFisrtname(),
+                user.getLastname(),
+                user.getPassword(),
+                user.getAddress(),
+                user.getGender()
+        );
+    }
+
+    public UserDto toDto(AppUser user) {
         UserDto userDto = new UserDto();
         userDto.setId(user.getId());
         userDto.setLastname(user.getLastname());
@@ -41,9 +53,22 @@ public class UserMapper {
         return userDto;
     }
 
+    public AppUser toEntityRegister(RegisterDTO registerDTO) {
+        AppUser user = new AppUser();
+        user.setUsername(registerDTO.getUsername());
+        user.setEmail(registerDTO.getEmail());
+        user.setFisrtname(registerDTO.getFirstname());
+        user.setLastname(registerDTO.getLastname());
+        user.setPassword(registerDTO.getPassword());
+        user.setAddress(registerDTO.getAddress());
+        user.setGender(registerDTO.getGender());
 
-    public User toEntity(UserDto userDto) {
-        User user = new User();
+        return user;
+    }
+
+
+    public AppUser toEntity(UserDto userDto) {
+        AppUser user = new AppUser();
         user.setId(userDto.getId());
         user.setLastname(userDto.getLastname());
         user.setFisrtname(userDto.getFisrtname());
@@ -54,13 +79,13 @@ public class UserMapper {
 
         if (userDto.getAddressId() != null) {
             Address address = addressDao.findById(userDto.getAddressId())
-                    .orElseThrow(() -> new RuntimeException("Category not found"));
+                    .orElseThrow(() -> new RuntimeException("Address not found"));
             user.setAddress(address);
         }
 
         if (userDto.getGenderId() != null) {
             Gender gender = genderDao.findById(userDto.getGenderId())
-                    .orElseThrow(() -> new RuntimeException("Category not found"));
+                    .orElseThrow(() -> new RuntimeException("Gender not found"));
             user.setGender(gender);
         }
         return user;
